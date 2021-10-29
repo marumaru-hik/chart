e7b2a8deb44f330f236422a4729c34f71fcc25bc
marumaru-hik <86957629+marumaru-hik@users.noreply.github.com>
2021-10-29 12:34:56 +0900

    Add files via upload

---
+++ count6.js
@@ -0,0 +1,32 @@
+let counterOutput = document.getElementById("counterOutput");
+
+// 加算機能
+let plusButton = document.getElementById("plusButton");
+let onClickPlusButton = () => {
+  if(counterOutput.value <= 27){
+    counterOutput.value++;
+  }
+};
+plusButton.addEventListener("click", onClickPlusButton);
+
+//  減算機能
+let minusButton = document.getElementById("minusButton");
+let onClickMinusButton = () => {
+  if(counterOutput.value >= 1){
+    counterOutput.value--;
+  }
+};
+minusButton.addEventListener("click", onClickMinusButton);
+
+// リセット
+let resetButton = document.getElementById("resetButton");
+let onClickResetButton = () => {
+  counterOutput.value = 0;
+};
+resetButton.addEventListener("click", onClickResetButton);
+
+var hid = document.getElementById('hid');
+
+if(counterOutput.value === 28){
+   hid.remove("hid");
+}
